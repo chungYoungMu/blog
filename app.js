31711e65df23b3db9ef668cfde634723080428b1
yang <895102025@qq.com>
2018-12-14 20:50:14 +0800

    登录与注册基本跳转

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.use("/node_modules",express.static("./node_modules"))
 app.get("/",(req,res)=>{
     res.render("index",{})
 })
+app.get("/register",(req,res)=>{
+    res.render("user/register",{})
+})
+
+app.get("/login",(req,res)=>{
+    res.render("user/login",{})
+})
 
 app.listen(80,()=>{
     console.log("http://127.0.0.1");
