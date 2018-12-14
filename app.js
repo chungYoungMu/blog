854a9642dd7fc8ab7e8386d6f41c596451981eb8
yang <895102025@qq.com>
2018-12-14 20:28:10 +0800

    cccc

---
+++ app.js
@@ -0,0 +1,14 @@
+const express =require('express')
+const app =express()
+
+app.set("view engine","ejs")
+
+app.use("/node_modules",express.static("./node_modules"))
+
+app.get("/",(req,res)=>{
+    res.render("index",{})
+})
+
+app.listen(80,()=>{
+    console.log("http://127.0.0.1");
+})
